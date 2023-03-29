9ff54342f3eef1fb8d086982ef4b53cb8753184c
Max2github <Your@email>
2023-03-29 19:11:43 +0200

    update c++ interface

--- lib/msh.cpp
+++ lib/msh.cpp
@@ -1,17 +1,12 @@
 #include "msh.hpp"
+
 #include "../include/alg.h"
 #include "../include/msh.h"
 
-msh::msh() {
-    
-}
-msh::~msh() {
-    msh_freeRessources();
-}
+msh::msh() {}
+msh::~msh() { msh_freeRessources(); }
 
-void msh::Script(char * script) {
-    msh_readScript(script);
-}
+void msh::Script(char * script) { msh_readScript(script); }
 
 // inline class
 class msh::execute::execinfo : public msh_info {
@@ -21,11 +16,11 @@ class msh::execute::execinfo : public msh_info {
         execinfo() {
             // *this = MSH_INFO_DEFAULT;
             this->wert = msh_Wert;
-            this->info = MSH_CALLINFO_DEFAULT;
+            this->info = (msh_callinfo) MSH_CALLINFO_DEFAULT;
             this->stack = (FUNC_LOCAL_STACK *) NULL;
             this->refs = NULL;
-            this->io = MSH_IO_DEFAULT;
-            this->event = MSH_EVENTS_DEFAULT;
+            this->io = (msh_io) MSH_IO_DEFAULT;
+            this->event = (msh_events) MSH_EVENTS_DEFAULT;
 
             this->in_func_old = false;
         }
@@ -37,13 +32,36 @@ class msh::execute::execinfo : public msh_info {
         void enter_func(const char * name) {
             this->in_func_old = this->info.in_func;
             this->info.in_func = true;
+            IN_FUNC = true; // this should be abolished / replaced by msh_info::info.in_func
             msh_func_deph_add_func(this, name);
         }
         void exit_func() {
+            IN_FUNC = in_func_old; // this should be abolished / replaced by msh_info::info.in_func
             this->info.in_func = in_func_old;
             msh_func_depth_remove_last_func(this);
         }
 };
+
+// manage global vars
+
+bool msh::var::Push(const char * name, const char * value) { return msh_var_push(NULL, value, name); }
+bool msh::var::Update(const char * name, const char * value) { return msh_var_updateByName(NULL, value, name); }
+bool msh::var::Update(unsigned int index, const char * value) { return msh_var_updateByIndex(NULL, value, index); }
+
+const char * msh::var::Get(const char * name) { return msh_var_getByName(NULL, name); }
+const char * msh::var::Get(unsigned int index) { return msh_var_getByIndex(NULL, index); }
+unsigned int msh::var::GetIndex(const char * name) { return msh_var_getIndexByName(NULL, name); }
+
+int msh::var::Count() {
+    return VAR_WORTZAELER;
+}
+void msh::var::PrintAll() {
+    for (int i = 0; i < VAR_WORTZAELER; i++) {
+        printf("%d | %s | %s\n", i, VAR_NAMES[i], VAR_SPEICHER[i]);
+    }
+}
+
+// execute
 
 msh::execute::execute() {
     this->msh = new execinfo();
@@ -60,6 +78,7 @@ msh::execute::~execute() {
 }
 
 void msh::execute::Line(const char * ln) { msh_readZeile(this->msh, ln); }
+void msh::execute::Script(char * script) { msh_readScript(script); }
 
 void msh::execute::Error(const char * msg) { return msh_error(this->msh, msg); }
 
@@ -67,11 +86,85 @@ unsigned int msh::execute::Info::Line() const { return this->msh->info.line; }
 bool msh::execute::Info::InFunc() const { return this->msh->info.in_func; }
 bool msh::execute::Info::InThread() const { return this->msh->info.in_thread; }
 
-bool msh::execute::Var::Push(const char * name, const char * value) { return msh_var_push(this->msh, value, name); }
-bool msh::execute::Var::Update(const char * name, const char * value) { return msh_var_updateByName(this->msh, value, name); }
-bool msh::execute::Var::Update(unsigned int index, const char * value) { return msh_var_updateByIndex(this->msh, value, index); }
+// Var
+
+bool msh::execute::Var::Push(const char * name, const char * value) {
+    if (this->msh->info.in_func) {
+        msh_func_update_local_Var(name, value, this->msh->stack);
+        return true;
+    }
+    return msh_var_push(this->msh, value, name);
+}
+bool msh::execute::Var::Update(const char * name, const char * value) {
+    if (this->msh->info.in_func) {
+        msh_func_update_local_Var(name, value, this->msh->stack);
+        return true;
+    }
+    return msh_var_updateByName(this->msh, value, name);
+}
+
 const char * msh::execute::Var::Get(const char * name) const { return msh_var_getByName(this->msh, name); }
 const char * msh::execute::Var::Get(unsigned int index) const { return msh_var_getByIndex(this->msh, index); }
-unsigned int msh::execute::Var::GetIndex(const char * name) const { return msh_var_getIndexByName(this->msh, name); }
+
+int msh::execute::Var::Count() const {
+    if (this->msh->info.in_func) {
+        if (this->msh->stack == NULL) { return -1; }
+        return s_arr_len(this->msh->stack->VAR_NAMES);
+    }
+    return msh::var::Count();
+}
+void msh::execute::Var::PrintAll() const {
+    if (this->msh->info.in_func) {
+        int nvars = this->Count();
+        if (nvars == -1) { puts("No stack!"); }
+        if (nvars == 0) { puts("No vars!"); }
+        for (int i = 0; i < nvars; i++) {
+            superstring name = s_arr_getEl(this->msh->stack->VAR_NAMES, i);
+            superstring value = s_arr_getEl(this->msh->stack->VAR_SPEICHER, i);
+            printf("%d | ", i); // s_printf("%SU | %SU \n", name, value); // bad - meory leaks
+            s_print(name); printf(" | ");
+            s_print(value); putchar('\n');
+        }
+    }
+    return msh::var::PrintAll();
+}
+
+// Func
+
+msh::execute::Func::Func() : entered_stack(NULL), old_stack(NULL) {}
 
 void msh::execute::Func::Push(const char * name, const char * script) { /* does not do anything yet */ }
+void msh::execute::Func::Call(const char * name) { return msh_func_call(this->msh, name); }
+
+bool msh::execute::Func::Enter(const char * name) {
+    if (this->entered_stack != NULL) {
+        // does not support nested entering
+        return false;
+    }
+    this->old_stack = (void *) this->msh->stack;
+    this->msh->stack = new FUNC_LOCAL_STACK; // { NULL, NULL };
+    // printf("stackp : %lld\n", (unsigned long long) this->msh->stack);
+    this->msh->stack->VAR_NAMES = NULL;
+    this->msh->stack->VAR_SPEICHER = NULL;
+    this->msh->enter_func(name);
+
+    this->entered_stack = (void *) this->msh->stack;
+    return true;
+}
+bool msh::execute::Func::Exit() {
+    if (this->entered_stack == NULL) {
+        // needs to have a successfull this->Enter before
+        return false;
+    }
+    this->msh->exit_func();
+
+    s_arr_free(this->msh->stack->VAR_NAMES);
+    s_arr_free(this->msh->stack->VAR_SPEICHER);
+    delete this->msh->stack;
+
+    this->msh->stack = (FUNC_LOCAL_STACK *) this->old_stack;
+    // printf("stackp : %lld\n", (unsigned long long) this->msh->stack);
+    this->entered_stack = NULL;
+    this->old_stack = NULL;
+    return true;
+}
--- lib/msh.hpp
+++ lib/msh.hpp
@@ -1,8 +1,26 @@
 #ifndef MSH_HPP
 #define MSH_HPP
 
+#ifndef MSH_ALLOW_UNSAFE
+    #define MSH_ALLOW_UNSAFE 0
+#endif
+
 class msh {
     public:
+        // this is basically a namespace
+        class var {
+            public:
+                static bool Push(const char * name, const char * value);
+                static bool Update(const char * name, const char * value);
+                static bool Update(unsigned int index, const char * value);
+
+                static const char * Get(const char * name);
+                static const char * Get(unsigned int index);
+                static unsigned int GetIndex(const char * name);
+
+                static int Count();
+                static void PrintAll();
+        };
         class execute {
             private:
                 class execinfo; // defined in msh.cpp
@@ -13,6 +31,14 @@ class msh {
 
                 // execute
                 void Line(const char *);
+     
+                /**
+                 * @brief Executes a msh-script
+                 * This functions creates and destroys its own msh::execute,
+                 * that's why it's static.
+                 * It's only also here, because I think people would expect it to be here.
+                 */
+                static void Script(char *);
 
                 // other helpfull functions
                 void Error(const char *);
@@ -25,7 +51,6 @@ class msh {
                         execinfo * msh;
                         friend class msh::execute;
                     public:
-
                         unsigned int Line() const;
                         bool InFunc() const;
                         bool InThread() const;
@@ -38,20 +63,45 @@ class msh {
                     public:
                         bool Push(const char * name, const char * value);
                         bool Update(const char * name, const char * value);
-                        bool Update(unsigned int index, const char * value);
 
                         const char * Get(const char * name) const;
                         const char * Get(unsigned int index) const;
-                        unsigned int GetIndex(const char * name) const;
+
+                        int Count() const;
+                        void PrintAll() const;
                 };
 
                 class Func {
                     private:
                         execinfo * msh;
                         friend class msh::execute;
-                    public:
+
+                        // used for Enter and Exit
+                        void * old_stack;
+                        void * entered_stack;
+                    private:
+                        Func();
+                    public :
+                        
+                        // proper function management
+                        // define a function - does nothing for now
                         void Push(const char * name, const char * script);
+                        // call a function
                         void Call(const char * name);
+
+                        // "fake" function management
+                        /**
+                         * @brief Enter in a function state
+                         * This does not define or call a function!!!
+                         * It just prepare an environment, like in a function
+                         */
+                        bool Enter(const char * name = "fakefunc");
+                        /**
+                         * @brief Exits the current function state
+                         * Only used, when this->Enter was used before!
+                         * Otherwise this may cause trouble.
+                         */
+                        bool Exit();
                 };
             public:
                 Info info;
@@ -64,10 +114,10 @@ class msh {
         
         /**
          * @brief Executes a msh-script
-         * This functions creates and destroys its own msg::execute,
+         * This functions creates and destroys its own msh::execute,
          * that's why it's here.
          */
-        void Script(char *);
+        static void Script(char *);
 };
 
 #endif
